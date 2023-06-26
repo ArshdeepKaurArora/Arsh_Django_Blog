07dcde51a2ac5e08f3a55037af75adf8da519243
ArshdeepKaurArora <kaurarshdeep0103@gmail.com>
2023-06-27 01:48:26 +0530

    Create Author and Tag table

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from datetime import date
 
-from .models import Post
+from .models import Post, Author, Tag
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
 
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Post(models.Model):
-    """Create a table for posts."""
+    """To create a model of post"""
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=250)
     image_name = models.CharField(max_length=100)
-    date = models.CharField(max_length=20)
-    slug = models.CharField(max_length=100)
-    content = models.CharField(max_length=500)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True,default="",null=False,db_index=True)
+    content = models.TextField([MinLengthValidator(250)])
+    author = models.ForeignKey("Author", null=True,
+                               on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField("Tag", related_name="posts")
 
     def save(self,*args,**kwargs):
         self.slug = slugify(self.title)
         super().save(*args,**kwargs)
+
+class Author(models.Model):
+    """To create a model of author"""
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=150)
+
+class Tag(models.Model):
+    """To create a model of tag."""
+    tag = models.CharField(max_length=50)
