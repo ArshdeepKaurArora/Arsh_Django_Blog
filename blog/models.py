ce000c1eb3d503b92a12d42b822889447bc72281
ArshdeepKaurArora <kaurarshdeep0103@gmail.com>
2023-06-27 00:17:25 +0530

    creating post table

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Post(models.Model):
+    """Create a table for posts."""
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=250)
+    image_name = models.CharField(max_length=100)
+    date = models.CharField(max_length=20)
+    slug = models.CharField(max_length=100)
+    content = models.CharField(max_length=500)
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args,**kwargs)
