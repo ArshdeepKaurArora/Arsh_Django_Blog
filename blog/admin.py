5ae41fc459ad1f0f0ed3a818037261aefc1ba034
ArshdeepKaurArora <kaurarshdeep0103@gmail.com>
2023-06-27 15:19:36 +0530

    Complete model work for blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from datetime import date
-
 from .models import Post, Author, Tag
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-
+    list_display = ("title", "author", "date",)
+    list_filter = ("author","date","tags",)
 
 # Register your models here.
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True,default="",null=False,db_index=True)
-    content = models.TextField([MinLengthValidator(250)])
+    content = models.TextField(validators=[MinLengthValidator(250)])
     author = models.ForeignKey("Author", null=True,
                                on_delete=models.SET_NULL, related_name="posts")
     tags = models.ManyToManyField("Tag", related_name="posts")
@@ -25,6 +25,12 @@ class Author(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=150)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Tag(models.Model):
     """To create a model of tag."""
     tag = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.tag}"
