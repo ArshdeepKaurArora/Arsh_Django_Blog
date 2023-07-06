566074ca7177ef870d6e988c77ca73634c129abe
ArshdeepKaurArora <kaurarshdeep0103@gmail.com>
2023-07-06 13:01:20 +0530

    Adding static files

--- my_site/settings.py
+++ my_site/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-vi_-el#vvulf(xxxje$*dygb%wd5o6%+=(0wp%1!#jnda*s(jb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST","127.0.0.1")
+]
 
 
 # Application definition
@@ -77,8 +79,11 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'arsh_blog',
+        'PASSWORD': '2Awsnikonita',
+        
     }
 }
 
